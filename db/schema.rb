260ca412b7d9e07d228796764b7efa031a456aa8
Ayan Saha <ayan.saha91@gmail.com>
2016-07-07 01:23:29 +0530

    action-cable-example code

--- db/schema.rb
+++ db/schema.rb
@@ -13,17 +13,14 @@
 
 ActiveRecord::Schema.define(version: 4) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "chatrooms", force: :cascade do |t|
+  create_table "chatrooms", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "topic"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "slug"
   end
 
-  create_table "messages", force: :cascade do |t|
+  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "content"
     t.integer  "user_id"
     t.integer  "chatroom_id"
@@ -34,7 +31,7 @@ ActiveRecord::Schema.define(version: 4) do
   add_index "messages", ["chatroom_id"], name: "index_messages_on_chatroom_id", using: :btree
   add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
